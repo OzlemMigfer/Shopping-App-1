164b0d184f65daf942bbe2f9a5949af99a24a4e7
Your Name <ozlemhayalkurun@gmail.com>
2022-10-18 18:05:31 +0300

    add design
    add design

---
+++ shoppingApp/src/components/ProductCard/ProductCard.styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        backgroundColor:'#d1d1e0',
+        borderColor:'#b3b3cb',
+        borderWidth:1,
+        margin:10,
+        flexDirection:'row'
+    },
+    image:{
+        minHeight:100,
+        width:100,
+        resizeMode:'contain',
+        backgroundColor:'white'
+    },
+    body_container:{
+        flex:1,
+        justifyContent:'space-between',
+        padding:10
+    },
+    title:{
+        fontSize:20,
+        color:'black'
+    },
+    price:{
+        textAlign:'right',
+        fontSize:20,
+        fontStyle:'italic'
+    }
+});
--- shoppingApp/src/pages/Products/Products.js
+++ shoppingApp/src/pages/Products/Products.js
@@ -1,12 +1,28 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React, {useState,useEffect} from "react";
+import {Text, View, FlatList} from 'react-native';
 import styles from './Products.styles';
 import Config from '../../../config';
+import axios from "axios";
+import ProductCard from '../../components/ProductCard';
 
 const Products = () => {
+    const[data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+    //verileri çekerken promise yapısını kullanıyoruz
+    const fetchData = async () => {
+        const {data: productData} = await axios.get(Config.API_URL);
+        setData(productData);
+    };
+
+    const renderProduct = ({item}) => <ProductCard product={item} />;
+
     return(
         <View>
-            <Text>PRODUCTS {Config.API_URL} </Text>
+            <FlatList data={data} renderItem={renderProduct}/>
         </View>
     );
 };
